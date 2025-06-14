044df2aa396cef50ea60b267b7efb31ea603bb3f
Richard Qumsieh <richardqumsieh@gmail.com>
2025-06-14 21:28:13 +0300

    Deleted Testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import { Hero } from '@/components/hero';
 import { Services } from '@/components/services';
 import { Contact } from '@/components/contact';
-import { Testimonials } from '@/components/testimonials';
 import { Header } from '@/components/header';
 import { Footer } from '@/components/footer';
 import { Hobbies } from '@/components/hobbies';
@@ -13,7 +12,6 @@ export default function Home() {
       <Hero />
       <Services />
       <Hobbies />
-      <Testimonials />
       <Contact />
       <Footer />
     </main>
